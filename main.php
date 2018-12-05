1fd598802ab57e5ef21d4b6f5e43ee1f66b6b437
tsokkisl <l3n05.ts@gmail.com>
2018-12-05 22:52:47 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,12 +1,12 @@
 <!--- All Tasks Component --->
 <div id="rightContainer">
-	<h3 style="text-align: center;margin-bottom: 20px;">All Tasks</h3>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-9">
-				<?php displayAssignments('all');?>
-				<a href="#menu-toggle" style="width:100px;" class="btn btn-secondary" id="menu-toggle">Menu</a>
-			</div>
-		</div>
-	</div>
-	<div>
+  <h3 style="text-align: center;margin-bottom: 20px;">All Tasks</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9">
+	<?php displayAssignments('all');?>
+	<a href="#menu-toggle" style="width:100px;" class="btn btn-secondary" id="menu-toggle">Menu</a>
+      </div>
+    </div>
+  </div>
+<div>
