199351f2d746449d3ccc9db583277cb1fc96eb51
tsokkisl <l3n05.ts@gmail.com>
2018-12-05 22:54:53 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,14 +1,13 @@
 <!--- Search Results Component --->
 <div id="rightContainer">
-	<h3 style="text-align: center;margin-bottom: 20px;">Search Results</h3>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-9">
-				<?php displayAssignments('search');?>
-				<a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Menu</a>
-				<a href="index.php"><button type="button" style="width:200px;"class="btn btn-primary">Back</button></a>
-			</div>
-		</div>
-	</div>
-
+  <h3 style="text-align: center;margin-bottom: 20px;">Search Results</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9">
+        <?php displayAssignments('search');?>
+	<a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Menu</a>
+	<a href="index.php"><button type="button" style="width:200px;"class="btn btn-primary">Back</button></a>
+      </div>
+    </div>
+  </div>
 <div>
