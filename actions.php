41a1b1dce3bce5f6761588c8835129ef468811eb
tsokkisl <l3n05.ts@gmail.com>
2018-12-05 22:34:24 +0200

    Update actions.php

--- actions.php
+++ actions.php
@@ -5,31 +5,31 @@ include("functions.php");
 //Submit a task
 if ($_GET['action'] == "submit") {
 
-	$error = "";
-	if (!$_POST['course']) {
-		$error = "Course is required.<br>";
-	}
-	if (!$_POST['info']) {
-		$error .= "Info is required.<br>";
-	}
-	if (!$_POST['date']) {
-		$error .= "Valid date is required";
-	}
+  $error = "";
+  if (!$_POST['course']) {
+    $error = "Course is required.<br>";
+  }
+  if (!$_POST['info']) {
+    $error .= "Info is required.<br>";
+  }
+  if (!$_POST['date']) {
+    $error .= "Valid date is required";
+  }
 
-	if ($error != "") {
-		echo $error;
-		exit();
-	}
+  if ($error != "") {
+    echo $error;
+    exit();
+  }
 
-	$query = "INSERT INTO `tasks` (`course`,`info`,`date`,`expired`) VALUES ('".mysqli_real_escape_string($link,$_POST['course'])."','".mysqli_real_escape_string($link,$_POST['info'])."',
+  $query = "INSERT INTO `tasks` (`course`,`info`,`date`,`expired`) VALUES ('".mysqli_real_escape_string($link,$_POST['course'])."','".mysqli_real_escape_string($link,$_POST['info'])."',
 	'".mysqli_real_escape_string($link,$_POST['date'])."','0')";
 
-	if (mysqli_query($link,$query)) {
-		echo 1;
-	}
-	else {
-		$error = "There was an error ... Please try again later";
-	}
+  if (mysqli_query($link,$query)) {
+    echo 1;
+  }
+  else {
+    $error = "There was an error ... Please try again later";
+  }
 
 }
 
@@ -37,27 +37,27 @@ if ($_GET['action'] == "submit") {
 //delete a task
 if ($_GET['action'] == 'delete') {
 
-	$query = "SELECT * FROM `tasks` WHERE id=".$_POST['taskId']." LIMIT 1";
-	$result = mysqli_query($link,$query);
+  $query = "SELECT * FROM `tasks` WHERE id=".$_POST['taskId']." LIMIT 1";
+  $result = mysqli_query($link,$query);
 
-	if (mysqli_num_rows($result) > 0 ) {
-		$row = mysqli_fetch_assoc($result);
-		mysqli_query($link,"DELETE FROM `tasks` WHERE id = ".mysqli_real_escape_string($link,$row['id'])." LIMIT 1");
-		echo "0";
-	}
-	else {
-		echo "1";
-	}
+  if (mysqli_num_rows($result) > 0 ) {
+    $row = mysqli_fetch_assoc($result);
+    mysqli_query($link,"DELETE FROM `tasks` WHERE id = ".mysqli_real_escape_string($link,$row['id'])." LIMIT 1");
+    echo "0";
+  }
+  else {
+    echo "1";
+  }
 
 }
 
 // set a task into expired
 if ($_GET['action'] == 'expired') {
 
-	$query = "UPDATE `tasks` SET expired = '1' WHERE id = ".$_POST['id']." ";
-	if (mysqli_query($link,$query)) {
-		echo 1;
-	}
+  $query = "UPDATE `tasks` SET expired = '1' WHERE id = ".$_POST['id']." ";
+  if (mysqli_query($link,$query)) {
+    echo 1;
+  }
 
 }
 ?>
