0ecd8d918d5b5ad7998f06ad4a77f578c5c41ecd
tsokkisl <l3n05.ts@gmail.com>
2018-12-05 22:58:45 +0200

    Update header.php

--- header.php
+++ header.php
@@ -18,10 +18,9 @@
   <!-- Required scripts to load first -->
   <script src="js/countdown.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
 </head>
-<body>
 
+<body>
   <div id="wrapper">
     <!-- Menu -->
     <div id="menu-wrapper">
